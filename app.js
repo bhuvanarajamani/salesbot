984f214598106e6fb4c718e4cd283b3e3e7d5b29
Bhuvana Rajamani <bhuvana.cr@gmail.com>
2016-08-24 21:17:28 -0500

    added ticket information

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ var connector = new builder.ChatConnector
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
+//default...
+/*
 bot.dialog('/', [
     function (session) {
         builder.Prompts.text(session, 'Hi! What is your name?');
@@ -27,6 +29,60 @@ bot.dialog('/', [
         session.send('Hello %s!', results.response);
     }
 ]);
+*/
+
+var ticketData = {
+    "jan": {
+        inflow: 2821,
+        resolved: 2464,
+        backlog: 1358
+    },
+    "feb": {
+        inflow: 2997,
+        resolved: 2717,
+        backlog: 1638
+    },
+    "mar": {
+        inflow: 3037,
+        resolved: 2922,
+        backlog: 1753
+    },
+    "apr": {
+        inflow: 3374,
+        resolved: 3442,
+        backlog: 1685
+    },
+    "may": {
+        inflow: 2731,
+        resolved: 3143,
+        backlog: 1273
+    },
+    "jun": {
+        inflow: 2467,
+        resolved: 2517,
+        backlog: 1223
+    },
+    "jul": {
+        inflow: 2247,
+        resolved: 2424,
+        backlog: 1046
+    }
+};
+
+bot.dialog('/', [
+    function (session) {
+        builder.Prompts.choice(session, "Hi. I am your tiketbot to help you with theticket metrics. Which month would you like ticket metrics for?", ticketData); 
+    },
+    function (session, results) {
+        if (results.response) {
+            var month = ticketData[results.response.entity];
+            session.send("There was an inflow of %(inflow)d tickets. Team resolved %(resolved)d and ended up with a backlog of %(backlog)d.", month); 
+        } else {
+            session.send("ok");
+        }
+    }
+]);
+
 
 server.get('/', restify.serveStatic({
  directory: __dirname,
